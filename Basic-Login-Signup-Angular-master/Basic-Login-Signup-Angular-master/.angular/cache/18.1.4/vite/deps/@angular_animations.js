13aa18b252af324b3e2231c7f2adb34fd57c0df5
abhay0933 <abhay80912@gmail.com>
2024-08-30 10:43:45 +0530

    changes

---
+++ Basic-Login-Signup-Angular-master/Basic-Login-Signup-Angular-master/.angular/cache/18.1.4/vite/deps/@angular_animations.js
@@ -0,0 +1,49 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-TTTXE2FT.js";
+import "./chunk-26ZBG2U3.js";
+import "./chunk-HZEFKQQ5.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
